1ac46df501c4cc843c483932927b6a88b08747ea
ablahum <mriztama@gmail.com>
2023-04-11 05:42:26 +0700

    refacto login page

---
+++ client/src/components/Form/style.js
@@ -0,0 +1,9 @@
+import { Button } from 'react-bootstrap'
+import styled from 'styled-components'
+
+export const SubmitBtn = styled(Button)`
+  color: #fff;
+  width: 100%;
+  font-weight: 600;
+  border: none;
+`
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import { Link, Navigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
-import { Container, Form } from 'react-bootstrap'
 
 import { roleChanges, userIdChanges } from '../../app/myReducer/action'
-import { ShowErrors } from '../../utils'
-import { Wrapper, FormWrapper, SubmitBtn } from './style'
 import { login } from '../../apis/auth'
+import { Form, Modal } from '../../components'
+import { Wrapper, FormWrapper } from './style'
 
 const Login = () => {
   const [data, setData] = useState({
     email: '',
     password: '',
   })
-  const [errorMessages, setErrorMessages] = useState([])
+  const [messages, setMessages] = useState([])
+  const [show, setShow] = useState(false)
 
   const dispatch = useDispatch()
 
-  if (localStorage.getItem('token')) return <Navigate to='/' />
+  if (localStorage.getItem('token') && show === false) return <Navigate to='/' />
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -28,98 +28,74 @@ const Login = () => {
     if (email.length === 0 || password.length === 0) message = [...message, 'Email or password cannot be empty']
 
     if (message.length > 0) {
-      setErrorMessages(message)
+      setMessages(message)
     } else {
       try {
         const res = await login({ email, password })
 
-        alert(res.data.message)
+        setMessages([res.data.message])
+        setShow(true)
 
         localStorage.setItem('token', res.data.token)
         dispatch(roleChanges(res.data.user.role))
         dispatch(userIdChanges(res.data.user._id))
-
-        setErrorMessages([])
       } catch (err) {
         message = [...message, err.response.data.message]
-        setErrorMessages(message)
+        setMessages(message)
       }
     }
   }
 
-  const handleChanges = (e) => {
+  const handleChanges = (e) =>
     setData(() => ({
       ...data,
       [e.target.id]: e.target.value,
     }))
+
+  const handleClose = () => {
+    setShow(false)
+    setMessages([])
   }
 
   return (
     <Wrapper>
-      <Container>
-        <FormWrapper>
-          <h2 className='text-center fw-bold mb-4'>SIGN IN</h2>
+      <FormWrapper>
+        <h2 className='text-center fw-bold mb-4'>SIGN IN</h2>
 
-          <Form
-            action='/login'
-            method='post'
+        <Form
+          data={data}
+          messages={messages}
+          handleChanges={handleChanges}
+          handleSubmit={handleSubmit}
+        />
+
+        <p className='text-center mt-3 mb-0'>
+          Don't have an account?
+          <Link
+            to='/register'
+            className='text-decoration-none'
           >
-            <Form.Group className='mb-3'>
-              <Form.Label>EMAIL:</Form.Label>
+            {' '}
+            Sign Up{' '}
+          </Link>
+          now
+        </p>
+        <p className='text-center'>
+          <Link
+            to='/'
+            className='text-decoration-none'
+          >
+            ← Back to home
+          </Link>
+        </p>
+      </FormWrapper>
 
-              <Form.Control
-                type='email'
-                id='email'
-                placeholder='Your email...'
-                value={data.email}
-                onChange={(e) => handleChanges(e)}
-                className='h-50'
-              />
-            </Form.Group>
-
-            <Form.Group className='mb-3'>
-              <Form.Label>PASSWORD:</Form.Label>
-
-              <Form.Control
-                type='password'
-                id='password'
-                placeholder='Your password...'
-                value={data.password}
-                onChange={(e) => handleChanges(e)}
-                className='h-50'
-              />
-            </Form.Group>
-
-            {errorMessages.length > 0 && <ShowErrors errors={errorMessages} />}
-
-            <SubmitBtn
-              href='#'
-              className='mt-3'
-              onClick={(e) => handleSubmit(e)}
-            >
-              SIGN IN
-            </SubmitBtn>
-          </Form>
-
-          <p className='text-center mt-3 mb-0'>
-            Don't have an account?{' '}
-            <Link
-              to='/register'
-              className='text-decoration-none'
-            >
-              Sign Up
-            </Link>{' '}
-            now
-            <br></br>
-            <Link
-              to='/'
-              className='text-decoration-none'
-            >
-              ← Back to home
-            </Link>
-          </p>
-        </FormWrapper>
-      </Container>
+      <Modal
+        show={show}
+        handleClose={handleClose}
+        title={messages}
+        message='Happy shopping!'
+      />
     </Wrapper>
   )
 }
