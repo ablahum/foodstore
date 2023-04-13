046fa7b3b3b569f8dea59129e375151d41463311
ablahum <mriztama@gmail.com>
2023-04-13 14:38:02 +0700

    adding email validation using regex

--- client/src/utils/total.js
+++ client/src/utils/total.js
@@ -1,7 +1,17 @@
+const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+export const validateEmail = (params) => {
+  if (regex.test(params)) {
+    return true
+  } else {
+    return false
+  }
+}
+
 // export function subtotal(items) {
 // return items.reduce((acc, curr) => acc + curr.qty, 0);
 // }
 
 export const total = (items) => {
-  return items.reduce((acc, curr) => acc + curr.price * curr.qty, 0);
-};
+  return items.reduce((acc, curr) => acc + curr.price * curr.qty, 0)
+}
