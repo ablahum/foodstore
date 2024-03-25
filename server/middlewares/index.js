f339d0a3e02f94aa36cd7f4e708db76306bd78b6
ablahum <mriztama@gmail.com>
2024-03-25 22:54:48 +0700

    refacto

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,7 +1,7 @@
 const { getToken, policyFor } = require('../utils')
 const jwt = require('jsonwebtoken')
 const config = require('../config')
-const User = require('../app/models/User')
+const { User } = require('../app/models')
 
 function decodeToken() {
   return async function (req, res, next) {
