4fbb72381e31ba637ecf6f8d9e21efd2246f20ba
ablahum <mriztama@gmail.com>
2024-03-26 23:48:30 +0700

    refacto Product.jsx

--- client/src/apis/products.js
+++ client/src/apis/products.js
@@ -2,11 +2,15 @@ import axios from 'axios'
 import { config } from '../config'
 
 export const getAll = async () => {
-  return await axios.get(`${config.api_host}/api/products`)
+  return await axios.get(`${config.apiHost}/api/products`)
+}
+
+export const getSpecific = async (params) => {
+  return await axios.get(`${config.apiHost}/api/products${params}`)
 }
 
 export const createOne = async (payload) => {
-  return await axios.post(`${config.api_host}/api/products`, payload, {
+  return await axios.post(`${config.apiHost}/api/products`, payload, {
     headers: {
       'Content-type': 'multipart/form-data',
       Authorization: localStorage.getItem('token'),
@@ -15,7 +19,7 @@ export const createOne = async (payload) => {
 }
 
 export const updateOne = async (id, payload) => {
-  return await axios.put(`${config.api_host}/api/products/${id}`, payload, {
+  return await axios.put(`${config.apiHost}/api/products/${id}`, payload, {
     headers: {
       'Content-type': 'multipart/form-data',
       Authorization: localStorage.getItem('token'),
@@ -24,7 +28,7 @@ export const updateOne = async (id, payload) => {
 }
 
 export const deleteOne = async (id) => {
-  return await axios.delete(`${config.api_host}/api/products/${id}`, {
+  return await axios.delete(`${config.apiHost}/api/products/${id}`, {
     headers: {
       Authorization: localStorage.getItem('token'),
     },
