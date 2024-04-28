4b4cfaa36aa3ba20fb7c7a6a502082122bf49817
ablahum <mriztama@gmail.com>
2024-04-28 23:10:58 +0700

    refacto home page (products, tags, pagination, footer)

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import { Container } from 'react-bootstrap'
 
 import { Heading, Pagination, Tag, Product } from '../../components'
 import { getAll } from '../../apis/tags'
-import { Wrapper, Top } from './style'
 
 const Home = () => {
   const [tags, setTags] = useState([])
@@ -23,19 +22,17 @@ const Home = () => {
   }, [])
 
   return (
-    <Wrapper>
+    <>
       <Heading title='our menus' />
 
       <Container className='py-md-5 py-4'>
-        <Top>
-          <Pagination />
-
-          <Tag tags={tags} />
-        </Top>
+        <Tag tags={tags} />
 
         <Product />
+
+        <Pagination />
       </Container>
-    </Wrapper>
+    </>
   )
 }
 
