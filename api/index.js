6c886136eaf54f3898ca121ff0c65e5a824f5873
ablahum <ablahum@proton.me>
2025-05-28 02:16:00 +0700

    refactor

--- api/index.js
+++ api/index.js
@@ -3,24 +3,10 @@ const serverless = require('serverless-http')
 
 const connectDB = require('../server/db')
 
-// let handler
-
-// module.exports = async (event, context) => {
-//   await connectDB()
-
-//   if (!handler) {
-//     handler = serverless(app)
-//   }
-
-//   return handler(event, context)
-// }
-
-// module.exports = serverless(app)
-
 let handler
 
-module.exports.handler = async (event, context) => {
-  await connectDB() // pastikan koneksi dibuka sebelum Express digunakan
+module.exports = async (event, context) => {
+  await connectDB()
 
   if (!handler) {
     handler = serverless(app)
@@ -28,3 +14,5 @@ module.exports.handler = async (event, context) => {
 
   return handler(event, context)
 }
+
+module.exports = serverless(app)
