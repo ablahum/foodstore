d5f60c106e68c45646e0c8d58e03ccef8bf2892d
ablahum <ablahum@proton.me>
2025-05-28 02:41:58 +0700

    refactor

--- server/app.js
+++ server/app.js
@@ -44,6 +44,10 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   res.status(err.status || 500)
+  res.json({
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  })
 })
 
 module.exports = app
